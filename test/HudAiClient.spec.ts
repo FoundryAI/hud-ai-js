63e2dc5095fb7801c8fe04c45047fa8c92284b7a
Nick Gerner <hello@njgerner.com>
2017-08-22 09:09:02 -0400

    add spec for checking resources

--- test/HudAiClient.spec.ts
+++ test/HudAiClient.spec.ts
@@ -1,4 +1,5 @@
 
+
 process.env.NODE_ENV = 'test';
 
 import * as Chance from 'chance';
@@ -9,6 +10,14 @@ import {HudAiClient} from '../lib/HudAiClient';
 import {PersistentSession} from '../lib/sessions/PersistentSession';
 import {TokenManager} from '../lib/TokenManager';
 import {RequestManager} from '../lib/RequestManager';
+
+import {ArticleResource} from '../lib/resources/Article';
+import {ArticleHighlightResource} from '../lib/resources/ArticleHighlight';
+import {CompanyResource} from '../lib/resources/Company';
+import {DomainResource} from '../lib/resources/Domain';
+import {KeyTermResource} from '../lib/resources/KeyTerm';
+import {TextCorpusResource} from '../lib/resources/TextCorpus';
+import {UserResource} from '../lib/resources/User';
 
 const chance = new Chance();
 
@@ -23,10 +32,19 @@ class HudAiClientSpec {
         expect(client.apiSession).to.be.an.instanceOf(PersistentSession);
         expect(client.requestManager).to.be.an.instanceOf(RequestManager);
         expect(client.tokenManager).to.be.an.instanceOf(TokenManager);
+
         expect(client.getTokensPasswordGrant).to.be.a('function');
         expect(client.getTokensClientCredentialsGrant).to.be.a('function');
         expect(client.getTokensAuthorizationGrant).to.be.a('function');
         expect(client.getTokensRefreshGrant).to.be.a('function');
+
+        expect(client.article).to.be.an.instanceOf(ArticleResource);
+        expect(client.articleHighlight).to.be.an.instanceOf(ArticleHighlightResource);
+        expect(client.company).to.be.an.instanceOf(CompanyResource);
+        expect(client.domain).to.be.an.instanceOf(DomainResource);
+        expect(client.keyTerm).to.be.an.instanceOf(KeyTermResource);
+        expect(client.textCorpus).to.be.an.instanceOf(TextCorpusResource);
+        expect(client.user).to.be.an.instanceOf(UserResource);
     }
 
 }
