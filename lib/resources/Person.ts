e55f83d8de32cc6c7e6796cccc42e7000379afaa
Nick Gerner <hello@njgerner.com>
2018-05-24 08:19:29 -0400

    add lookup methods

--- lib/resources/Person.ts
+++ lib/resources/Person.ts
@@ -8,6 +8,7 @@ import {
 } from '../utils/Resource';
 import { RequestManager } from '../RequestManager';
 import { Person } from '../entities';
+import * as _ from 'lodash';
 
 export interface PersonListAttributes extends HudAiListAttributes {
     companyId?: string;
@@ -35,6 +36,19 @@ export interface PersonUpdateAttributes extends HudAiUpdateAttributes {
     twitterHandle?: string;
 }
 
+export interface PersonEmailLookupAttributes {
+    firstName: string;
+    lastName: string;
+    companyId?: string;
+    domain?: string;
+}
+
+export interface PersonLinkedinLookupAttributes {
+    firstName: string;
+    lastName: string;
+    companyId: string;
+}
+
 export class PersonResource extends Resource<Person, PersonListAttributes, PersonCreateAttributes, PersonUpdateAttributes> {
     constructor(requestManager: RequestManager) {
         super('/people', requestManager);
@@ -54,6 +68,22 @@ export class PersonResource extends Resource<Person, PersonListAttributes, Perso
 
     public update(id: string | number, updateArgs: PersonUpdateAttributes): Promise<Person> {
         return this._update(id, updateArgs);
+    }
+
+    public lookupEmail(lookupArgs: PersonEmailLookupAttributes): Promise<{ email: string|null, score: number }> {
+        return this.makeRequest({
+            method: 'POST',
+            data: lookupArgs,
+            url: `${this.basePath}/email-lookup`
+        })
+    }
+
+    public lookupLinkedin(lookupArgs: PersonLinkedinLookupAttributes): Promise<{ url: string|null }> {
+        return this.makeRequest({
+            method: 'POST',
+            data: lookupArgs,
+            url: `${this.basePath}/linkedin-lookup`
+        })
     }
 
     public del(id: string | number): Promise<void> {
