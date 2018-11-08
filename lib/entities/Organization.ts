dd49c5e0f3409c801ab5d7f5b9eda053355d799b
Peter <peter@foundry.ai>
2018-11-08 15:11:47 -0500

    create org feed setting resource

--- lib/entities/Organization.ts
+++ lib/entities/Organization.ts
@@ -19,3 +19,26 @@ export interface OrganizationUserRole {
     createdAt: Date;
     updatedAt: Date;
 }
+
+export interface OrganizationFeedSettings {
+    id: string;
+    organizationId: string;
+    importance: number|null;
+    article: number|null;
+    tweet: number|null;
+    quote: number|null;
+    stockAlert: number|null;
+    video: number|null;
+    businessWord: number|null;
+    feedContext: number|null;
+    followedPerson: number|null;
+    industryTerm: number|null;
+    jobFunctionTerm: number|null;
+    corpusTerm: number|null;
+    source: number|null;
+    minImportance: number|null;
+    decay: number|null;
+    decayOffset: string|null;
+    createdAt: Date;
+    updatedAt: Date;
+}
